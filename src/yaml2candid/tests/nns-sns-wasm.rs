2f3d0063c8234e00cd77bb3406ec1fc9bc88ecd2
Max <max@dfinity.org>
2022-11-10 15:00:43 +0100

    clippy

--- src/yaml2candid/tests/nns-sns-wasm.rs
+++ src/yaml2candid/tests/nns-sns-wasm.rs
@@ -7,7 +7,7 @@ fn should_convert_sns_yaml() {
     let converter = Yaml2Idl::from_did_file("tests/nns-sns-wasm.did").unwrap();
     let yaml_str = include_str!("nns-sns-wasm/SnsInitPayload.yaml");
     let idl_str = include_str!("nns-sns-wasm/SnsInitPayload.idl");
-    let yaml_value: YamlValue = serde_yaml::from_str(&yaml_str).unwrap();
+    let yaml_value: YamlValue = serde_yaml::from_str(yaml_str).unwrap();
     let idl = converter
         .convert(&IDLType::VarT("SnsInitPayload".to_string()), &yaml_value)
         .unwrap();
