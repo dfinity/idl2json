885f84ba5c6ef59b1d1833307ffa3b5c24381d97
Max <bitdivine@users.noreply.github.com>
2022-11-16 17:05:00 +0100

    ci: Add code coverage threshold (#11)
    # Motivation
    Code coverage should be high. Adding a threshold in CI should help
    enforce that.

--- src/idl2json/src/test.rs
+++ src/idl2json/src/test.rs
@@ -1,28 +1,36 @@
-use crate::{idl_to_serde, JsonValue};
+use crate::{idl2json, JsonValue};
 use candid::IDLArgs;
 use std::fs;
 
 /// Returns the absolute path to a file in the samples directory.
 macro_rules! sample_file {
-    ($file:literal) => {
-        concat!(env!("CARGO_MANIFEST_DIR"), "/../../samples/", $file)
+    ($file:ident) => {
+        format!("{}/../../samples/{}", env!("CARGO_MANIFEST_DIR"), $file)
     };
 }
 
-#[test]
-fn idl_is_parsed_as_expected() {
+fn idl_is_parsed_as_expected(idl_filename: &str, json_filename: &str) {
     let expected_json_string: String =
-        fs::read_to_string(sample_file!("proposal.json")).expect("Could not read sample JSON");
+        fs::read_to_string(sample_file!(json_filename)).expect("Could not read sample JSON");
     let expected_json_value: JsonValue =
         serde_json::from_str(&expected_json_string).expect("Could not parse sample JSON");
 
     let idl_string: String =
-        fs::read_to_string(sample_file!("proposal.idl")).expect("Could not read sample IDL");
+        fs::read_to_string(sample_file!(idl_filename)).expect("Could not read sample IDL");
     let idl_value: IDLArgs = idl_string.parse::<IDLArgs>().expect("Malformed input");
-    let json_value: JsonValue = idl_to_serde(&idl_value.args[0]);
+    let json_value: JsonValue = idl2json(&idl_value.args[0]);
 
     let diff = json_patch::diff(&expected_json_value, &json_value);
     if !(diff.0).is_empty() {
-        panic!("Unexpected changes:\n{:?}", diff);
+        panic!(
+            "Unexpected changes in {idl_filename} conversion:\n{:?}",
+            diff
+        );
     }
+}
+
+#[test]
+fn sample_idls_are_parsed_as_expected() {
+    idl_is_parsed_as_expected("proposal.idl", "proposal.json");
+    idl_is_parsed_as_expected("all_types.idl", "all_types.json");
 }
--- src/yaml2candid/tests/nns-sns-wasm.rs
+++ src/yaml2candid/tests/nns-sns-wasm.rs
@@ -14,3 +14,24 @@ fn should_convert_sns_yaml() {
     let actual_idl_str = idl.to_string();
     assert_eq!(&idl_str, &actual_idl_str);
 }
+
+#[test]
+fn should_convert_sns_yaml_strings() {
+    let converter = Yaml2Candid::from_did_file("tests/nns-sns-wasm.did").unwrap();
+    let yaml_str = include_str!("nns-sns-wasm/SnsInitPayload.yaml");
+    let expected_idl_str = include_str!("nns-sns-wasm/SnsInitPayload.idl");
+    let actual_idl_str = converter.convert_str("SnsInitPayload", yaml_str).unwrap();
+    assert_eq!(expected_idl_str, &actual_idl_str);
+}
+
+#[test]
+fn should_fail_if_absolute_did_file_is_missing() {
+    let converter = Yaml2Candid::from_did_file("/nonexistent.did");
+    assert!(converter.is_err());
+}
+
+#[test]
+fn should_fail_if_relative_did_file_is_missing() {
+    let converter = Yaml2Candid::from_did_file("tests/nonexistent.did");
+    assert!(converter.is_err());
+}
