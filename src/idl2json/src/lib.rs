9bf735b2afa4a10fdc1ec08f06be8db4fe2a2878
Max <max@dfinity.org>
2022-11-21 16:21:39 +0100

    Add a typed idl2json

--- src/idl2json/src/lib.rs
+++ src/idl2json/src/lib.rs
@@ -1,55 +1,7 @@
-use candid::parser::value::IDLValue;
-use serde_json::value::Value as JsonValue;
+mod typed_conversion;
+mod untyped_conversion;
+pub use untyped_conversion::idl2json;
+pub use typed_conversion::idl2json_with_weak_names;
+pub use serde_json::Value as JsonValue;
 #[cfg(test)]
 mod test;
-
-/// Converts a candid IDLValue to a serde JsonValue
-pub fn idl2json(idl: &IDLValue) -> JsonValue {
-    match idl {
-        IDLValue::Bool(bool) => JsonValue::Bool(*bool),
-        IDLValue::Null => JsonValue::Null,
-        IDLValue::Text(s) => JsonValue::String(s.clone()),
-        IDLValue::Number(s) => JsonValue::String(s.clone()), // Unspecified number type
-        IDLValue::Float64(f) => {
-            JsonValue::Number(serde_json::Number::from_f64(*f).expect("A float's a float"))
-        }
-        IDLValue::Opt(value) => JsonValue::Array(vec![idl2json(value)]),
-        IDLValue::Vec(value) => JsonValue::Array(value.iter().map(idl2json).collect()),
-        IDLValue::Record(value) => JsonValue::Object(
-            value
-                .iter()
-                .map(|field| (format!("{}", field.id), idl2json(&field.val)))
-                .collect(),
-        ),
-        IDLValue::Variant(field, _) => JsonValue::Object(
-            vec![(format!("{}", field.id), idl2json(&field.val))]
-                .into_iter()
-                .collect(),
-        ),
-        IDLValue::Principal(p) => JsonValue::String(format!("{}", p)),
-        IDLValue::Service(p) => JsonValue::String(format!("{}", p)),
-        IDLValue::Func(p, c) => JsonValue::Object(
-            vec![
-                ("principal".to_string(), JsonValue::String(format!("{}", p))),
-                ("code".to_string(), JsonValue::String(c.to_string())),
-            ]
-            .into_iter()
-            .collect(),
-        ),
-        IDLValue::None => JsonValue::Array(vec![]),
-        IDLValue::Int(i) => JsonValue::String(format!("{}", i)),
-        IDLValue::Nat(i) => JsonValue::String(format!("{}", i)),
-        IDLValue::Nat8(i) => JsonValue::Number(serde_json::Number::from(*i)),
-        IDLValue::Nat16(i) => JsonValue::Number(serde_json::Number::from(*i)),
-        IDLValue::Nat32(i) => JsonValue::Number(serde_json::Number::from(*i)),
-        IDLValue::Nat64(i) => JsonValue::String(format!("{}", i)),
-        IDLValue::Int8(i) => JsonValue::Number(serde_json::Number::from(*i)),
-        IDLValue::Int16(i) => JsonValue::Number(serde_json::Number::from(*i)),
-        IDLValue::Int32(i) => JsonValue::Number(serde_json::Number::from(*i)),
-        IDLValue::Int64(i) => JsonValue::String(format!("{}", i)),
-        IDLValue::Float32(f) => {
-            JsonValue::Number(serde_json::Number::from_f64(*f as f64).expect("A float's a float"))
-        }
-        IDLValue::Reserved => panic!("Unimplemented: {:?}", idl),
-    }
-}
