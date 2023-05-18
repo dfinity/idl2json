db2c88529b1f937d4b8c550cfe6441786bc423c9
Max Murphy-Skvorzov <max@winning.black>
2023-05-18 06:43:09 +0200

    More test vectors

--- src/idl2json_cli/src/tests.rs
+++ src/idl2json_cli/src/tests.rs
@@ -9,6 +9,8 @@ fn simple_conversion_should_be_correct() {
    let args = Args::default();
     let vectors = [
      TestVector{ stdin: "()", stdout: "\n" }
+     TestVector{ stdin: "( record {} )", stdout: "{}\n" }
+     TestVector{ stdin: "( record {}, record {} )", stdout: "{}\n{}\n" }
   ];
   for vector in vectors {
       let out = main(&args, vector.stdin).map_err(|e| anyhow!("Failed to parse: {} due to: {e}", vector.stdin)).unwrap();
