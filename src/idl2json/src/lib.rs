2469a5b1ca8d8efc247d8d70942f87635604ad67
Max Murphy-Skvorzov <max@winning.black>
2023-05-25 10:30:13 +0200

    Placeholders for type extraction functions

--- src/idl2json/src/lib.rs
+++ src/idl2json/src/lib.rs
@@ -12,7 +12,10 @@ mod typed_conversion;
 mod untyped_conversion;
 
 pub use serde_json::Value as JsonValue;
-pub use typed_conversion::{idl2json_with_weak_names, idl_args2json_with_weak_names};
+pub use typed_conversion::{
+    get_fn_type, get_service_arg, get_type, get_type_from_any, idl2json_with_weak_names,
+    idl_args2json_with_weak_names,
+};
 pub use untyped_conversion::{idl2json, idl_args2json};
 #[cfg(test)]
 mod test;
