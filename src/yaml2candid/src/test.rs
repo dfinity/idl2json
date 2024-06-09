999a5c26a9d0d0d8ae6c824ab0b54cd56836c6d8
Max Murphy <max@winning.black>
2024-06-09 14:27:29 +0200

    u16

--- src/yaml2candid/src/test.rs
+++ src/yaml2candid/src/test.rs
@@ -28,3 +28,25 @@ fn can_convert_u8() {
         assert_conversion_is(&converter, &typ, &data, expected_result);
     }
 }
+
+#[cfg(test)]
+fn can_convert_i16() {
+    let converter = Yaml2Candid::default();
+    let typ = IDLType::PrimT(candid_parser::types::PrimType::Int16);
+    for value in [i16::MIN, -1, 0, 1, i16::MAX].iter() {
+        let data = YamlValue::from(*value);
+        let expected_result = IDLValue::Int16(*value);
+        assert_conversion_is(&converter, &typ, &data, expected_result);
+    }
+}
+
+#[cfg(test)]
+fn can_convert_u16() {
+    let converter = Yaml2Candid::default();
+    let typ = IDLType::PrimT(candid_parser::types::PrimType::Nat16);
+    for value in [0, 1, u16::MAX].iter() {
+        let data = YamlValue::from(*value);
+        let expected_result = IDLValue::Nat16(*value);
+        assert_conversion_is(&converter, &typ, &data, expected_result);
+    }
+}
